c6f45930e035ee756266b478f6032dad297f998b
unknown <steamlol83@gmail.com>
2024-01-31 09:50:16 +0100

    Ajout
    -jeux2.py
    Programe va essayer de devinée votre nombre en 10 essaie

---
+++ jeux2.py
@@ -0,0 +1,36 @@
+import random
+
+nombrejoueur=0
+
+tries = 10
+secret=0
+
+maxNUM = 100
+minNUM = 0
+
+while True:
+    print("Donnée un nombre que le program va devinée")
+    secret = int(input())
+
+    for i in range(tries):
+        if i == 0:
+            print("Programe Veuillez saisir un nombre")
+
+        if nombrejoueur != secret:
+            nombrejoueur = random.randint(minNUM, maxNUM)
+
+        print(nombrejoueur)
+        if nombrejoueur == secret:
+            print("Bravo")
+            break
+        else:
+            if nombrejoueur > secret:
+                print("Le nombre est plus petit")
+                maxNUM = nombrejoueur-1
+            elif nombrejoueur < secret:
+                print("Le nombre est plus grand")
+                minNUM = nombrejoueur+1
+    
+    if nombrejoueur != secret:
+        print("Vous êtes tombée a cour d'essait, le nombre était", secret)
+    break
